4afdd77da9f04573a3c6bc5dc73719ce96f2607e
iagob28 <61669995+iagob28@users.noreply.github.com>
2022-02-16 11:30:07 -0300

    env variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,12 +5,12 @@ import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNIlzFar4F4srniXXuZ0GahYiVk1cDyv4",
-  authDomain: "moveit-c6764.firebaseapp.com",
-  projectId: "moveit-c6764",
-  storageBucket: "moveit-c6764.appspot.com",
-  messagingSenderId: "417246209797",
-  appId: "1:417246209797:web:b27b2425546935a590f96c",
+  apiKey: process.env.REACT_APP_VERCEL_API_KEY,
+  authDomain: process.env.REACT_APP_VERCEL_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_VERCEL_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_VERCEL_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_VERCEL_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_VERCEL_APP_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
